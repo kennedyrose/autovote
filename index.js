32e4d5ef2e3a422ab333f9e62bef0889587a64b5
Kennedy Rose <kennedyianrose@gmail.com>
2020-02-17 16:52:12 -0600

    Faster

--- index.js
+++ index.js
@@ -29,70 +29,76 @@ const block = [
 
 async function tick(){
 
-	// Launch browser
-	if(!browser){
-		console.log(`Launching browser...`)
-		browser = await puppeteer.launch(options)
-		page = await browser.newPage()
-		await page.setRequestInterception(true)
-		page.on('request', request => {
-			const type = request.resourceType()
+	try {
 
-			if (block.indexOf(type) > -1) {
-				request.abort()
-			}
-			else {
-				request.continue()
-			}
-		});
+		// Launch browser
+		if (!browser) {
+			console.log(`Launching browser...`)
+			browser = await puppeteer.launch(options)
+			page = await browser.newPage()
+			await page.setRequestInterception(true)
+			page.on('request', request => {
+				const type = request.resourceType()
+
+				if (block.indexOf(type) > -1) {
+					request.abort()
+				}
+				else {
+					request.continue()
+				}
+			});
+		}
+
+		console.log(`Generating random user agent...`)
+		await page.setUserAgent(getRandom())
+
+		console.log(`Loading page...`)
+		page.goto('https://www.courierpress.com/story/sports/high-school/polls/2020/02/17/who-your-turonis-high-school-athlete-week/4785043002/')
+
+
+		console.log(`Looking for input...`)
+		await page.waitForSelector(`input#PDI_answer48605260`)
+
+		console.log(`Clicking input...`)
+		await page.click(`input#PDI_answer48605260`)
+
+		console.log(`Clicking submit button...`)
+		await page.click(`#pd-vote-button10507350`)
+
+		console.log(`Waiting for results...`)
+		await page.waitForSelector(`.pds-feedback-group`)
+
+		const score = await page.evaluate(() => {
+			const str = []
+			const els = document.querySelectorAll(`.pds-feedback-group`)
+			const msg = document.querySelector(`.pds-question-top`).textContent
+			str.push(msg)
+			els.forEach(el => {
+				const name = el.querySelector(`.pds-answer-text`)
+					.textContent
+					.split(`,`)[0]
+				const votes = el.querySelector(`.pds-feedback-votes`)
+					.textContent
+					.split(`(`)[1]
+					.split(`)`)[0]
+				str.push(`${name}: ${votes}`)
+			})
+			return str.join(`\n`)
+		})
+
+		total++
+		const result = [
+			`Voted ${total} times`,
+			score,
+		]
+
+		console.log(`\n${result.join(`\n`)}\n`)
+	}
+	catch(err){
+		console.error(err)
 	}
 
-	console.log(`Generating random user agent...`)
-	await page.setUserAgent(getRandom())
 
-	console.log(`Loading page...`)
-	page.goto('https://www.courierpress.com/story/sports/high-school/polls/2020/02/17/who-your-turonis-high-school-athlete-week/4785043002/')
-
-
-	console.log(`Looking for input...`)
-	await page.waitForSelector(`input#PDI_answer48605260`)
-
-	await timer(3000)
-	console.log(`Clicking input...`)
-	await page.click(`input#PDI_answer48605260`)
-
-	console.log(`Clicking submit button...`)
-	await page.click(`#pd-vote-button10507350`)
-
-	console.log(`Waiting for results...`)
-	await page.waitForSelector(`.pds-feedback-group`)
-
-	const score = await page.evaluate(() => {
-		const str = []
-		const els = document.querySelectorAll(`.pds-feedback-group`)
-		const msg = document.querySelector(`.pds-question-top`).textContent
-		str.push(msg)
-		els.forEach(el => {
-			const name = el.querySelector(`.pds-answer-text`)
-				.textContent
-				.split(`,`)[0]
-			const votes = el.querySelector(`.pds-feedback-votes`)
-				.textContent
-				.split(`(`)[1]
-				.split(`)`)[0]
-			str.push(`${name}: ${votes}`)
-		})
-		return str.join(`\n`)
-	})
-
-	total++
-	const result = [
-		`Voted ${total} times`,
-		score,
-	]
-
-	console.log(`\n${result.join(`\n`)}\n`)
-	await timer(3000)
 	tick()
 }
 function timer(n) {
@@ -101,10 +107,5 @@ function timer(n) {
 	})
 }
 
-try {
-	tick()
-}
-catch(err){
-	console.error(err)
-	tick()
-}
+
+tick()
